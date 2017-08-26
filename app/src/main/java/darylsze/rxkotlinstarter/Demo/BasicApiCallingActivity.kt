353a2269b4f3756c4a6e128a019226549afada12
Daryl Sze <daryl.sze@hkmovie6.com>
2017-08-26 09:27:33 +0800

    Fix Demo activities

--- app/src/main/java/darylsze/rxkotlinstarter/Demo/BasicApiCallingActivity.kt
+++ app/src/main/java/darylsze/rxkotlinstarter/Demo/BasicApiCallingActivity.kt
@@ -1,13 +1,27 @@
 package darylsze.rxkotlinstarter.Demo
 
+import android.app.Activity
+import android.app.Application
+import android.app.Service
 import android.os.Bundle
+import android.util.AndroidException
+import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
 import com.google.gson.Gson
 import darylsze.rxkotlinstarter.Activity.BaseActivity
+import darylsze.rxkotlinstarter.Application.MainApplication
+import darylsze.rxkotlinstarter.Extension.MyCompositeDisposable
 import darylsze.rxkotlinstarter.Extension.catchAllExceptions
 import darylsze.rxkotlinstarter.R
+import darylsze.rxkotlinstarter.Retrofit.Plugins.InstantTypeConverter
+import darylsze.rxkotlinstarter.Retrofit.Service.ExampleApiService
 import darylsze.rxkotlinstarter.Retrofit.Service.ExampleApiServiceImpl
+import darylsze.rxkotlinstarter.Utils.CrossContextDialogManager
+import darylsze.rxkotlinstarter.Utils.ProgressDialogManager
+import retrofit2.Response
+import retrofit2.Retrofit
 import timber.log.Timber
+import java.security.cert.Extension
 
 class BasicApiCallingActivity : BaseActivity() {
     val exampleApiService by instance<ExampleApiServiceImpl>()
@@ -29,3 +43,4 @@ class BasicApiCallingActivity : BaseActivity() {
                 .subscribe()
     }
 }
+
